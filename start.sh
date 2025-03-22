087d458e251a97ddc346f7cb98da5b6c378cfd79
doctorxlovexat <ervin-star@hotmail.de>
2025-03-22 11:26:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Postavi potrebnu putanju do tvog icecast.xml fajla
-ICECAST_XML_PATH=/path/to/your/icecast.xml
-
-# Pokreće Icecast server sa odgovarajućim XML fajlom
-docker run -d -p 8000:8000 --name icecast \
-  -v $ICECAST_XML_PATH:/etc/icecast/icecast.xml \
-  my-icecast
+# Pokreće Icecast unutar Docker kontejnera
+docker build -t my-icecast .  # Izgradi Docker image (ako je potrebno)
+docker run -d -p 8000:8000 --name icecast my-icecast  # Pokreće kontejner u pozadini, izlažući port 8000
